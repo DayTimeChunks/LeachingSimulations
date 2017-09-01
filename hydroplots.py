6159f71a2254a996d8e5978197ee8e489b77b78a
pablo alvarez <pablo@ble.local>
2017-09-01 15:57:57 +0200

    results crop

--- hydroplots.py
+++ hydroplots.py
@@ -101,6 +101,223 @@ def hydroplot(data,
     plt.show()
 
 
+def extract_and_draw_hydrotable(
+        water_data,
+        series_name1, series_name2, series_name3, series_name4,
+        title,
+        soil,
+        isFirstCycle,
+        isPercolation
+):
+
+    """
+
+    :param water_data:
+    :param series_name1:
+    :param series_name2:
+    :param series_name3:
+    :param series_name4:
+    :param title:
+    :param soil:
+    :param isFirstCycle:
+    :param isPercolation:
+    :return:
+    """
+    if soil == 'Alteck':
+        if isFirstCycle:
+            # Observed percolation
+            # fresh, fresh, aged, aged
+            # all at 6 min, high inetnesity
+            leach_high_6min = np.array([16.253, 12.958, 17.536, 14.29])
+            # all at 12 min, med intensity
+            leach_med_12min = np.array([10.089, 5.902, 13.981, 10.602])
+            # all at 30min, med intensity
+            leach_med_30min = np.array([49.197, 40.402, 45.772, 47.201])
+            # all at 30min, low intensity
+            leach_low_30min = np.array([20.037, 17.508, 22.376, 20.085])
+
+
+            # Time
+            cum_time_30min = water_data[:, 0]
+            print(cum_time_30min)
+
+            # Cummulative infiltration
+            cum_inf_135mmh = water_data[:, 4]
+            cum_inf_55mmh = water_data[:, 5]
+            cum_inf_30mmh = water_data[:, 6]
+
+            # Cummulative leaching
+            cum_leach_135mmh = water_data[:, 7]
+            print(cum_leach_135mmh[-1])
+            cum_leach_55mmh = water_data[:, 8]
+            cum_leach_30mmh = water_data[:, 9]
+
+            # Ponding
+            roff_135mmh = water_data[:, 10]
+            roff_55mmh = water_data[:, 11]
+            roff_30mmh = water_data[:, 12]
+
+            # Cummulative ponding
+            cum_roff_135mmh = water_data[:, 13]
+            cum_roff_55mmh = water_data[:, 14]
+            cum_roff_30mmh = water_data[:, 15]
+
+            infil_135mmh = water_data[:, 16]
+            infil_55mmh = water_data[:, 17]
+            infil_30mmh = water_data[:, 18]
+
+            percol_data1 = stackdata3(cum_time_30min,
+                                      cum_leach_135mmh, cum_leach_55mmh, cum_leach_30mmh)
+
+            runoff_data1 = stackdata3(cum_time_30min,
+                                      cum_roff_135mmh, cum_roff_55mmh, cum_roff_30mmh)
+
+            infil_data1 = stackdata3(cum_time_30min,
+                                     infil_135mmh, infil_55mmh, infil_30mmh)
+
+            time_size_135mmh = water_data[:, 19]
+            time_size_55mmhA = water_data[:, 20]
+            time_size_55mmhB = water_data[:, 20]
+            time_size_30mmh = water_data[:, 21]
+
+            time_sizes1 = [time_size_135mmh, time_size_135mmh,
+                           time_size_55mmhA, time_size_55mmhA,
+                           time_size_55mmhB, time_size_55mmhB,
+                           time_size_30mmh, time_size_30mmh]
+
+            return hydroplot(percol_data1,
+                  series_name1, series_name2, series_name3,
+                  leach_high_6min,
+                  leach_med_12min, leach_med_30min,
+                  leach_low_30min,
+                  title)
+
+        else:
+            if isPercolation:
+                # Observed percolation
+                # Order if array is:
+                #  [sterile, untreat, sterile_aged, untreat_aged]
+
+                # At 6 min, high inetnesity
+                leach_high_6min = np.array([14.192, 8.245, 2.410, 5.469])
+                # At 12 min, med intensity
+                leach_med_12min = np.array([18.672, 19.0, 0.830, 11.407])
+                # At 30min, med intensity
+                leach_med_30min = np.array([12.697, 2.473, 3.52, 20.291])
+                # At 30min, low intensity
+                leach_low_30min = np.array([29.656, 9.375, 0.409, 3.385])
+
+                # Time axis
+                cum_time_30min = water_data[0][:, 0]
+
+                # Cumulative leachate
+                cum_leach_135mmh_SF = water_data[0][:, 2]
+                cum_leach_135mmh_SA = water_data[0][:, 4]
+                cum_leach_135mmh_LF = water_data[0][:, 6]
+                cum_leach_135mmh_LA = water_data[0][:, 8]
+
+                cum_leach_55mmhA_SF = water_data[0][:, 10]
+                cum_leach_55mmhA_SA = water_data[0][:, 12]
+                cum_leach_55mmhA_LF = water_data[0][:, 14]
+                cum_leach_55mmhA_LA = water_data[0][:, 16]
+
+                cum_leach_55mmhB_SF = water_data[0][:, 18]
+                cum_leach_55mmhB_SA = water_data[0][:, 20]
+                cum_leach_55mmhB_LF = water_data[0][:, 22]
+                cum_leach_55mmhB_LA = water_data[0][:, 24]
+
+                cum_leach_30mmh_SF = water_data[0][:, 26]
+                cum_leach_30mmh_SA = water_data[0][:, 28]
+                cum_leach_30mmh_LF = water_data[0][:, 30]
+                cum_leach_30mmh_LA = water_data[0][:, 32]
+
+                # Group each compartment for graphing
+                percol_data2 = stackdata16(
+                    cum_time_30min,
+                    cum_leach_135mmh_SF, cum_leach_55mmhA_SF, cum_leach_55mmhB_SF, cum_leach_30mmh_SF,
+                    cum_leach_135mmh_SA, cum_leach_55mmhA_SA, cum_leach_55mmhB_SA, cum_leach_30mmh_SA,
+                    cum_leach_135mmh_LF, cum_leach_55mmhA_LF, cum_leach_55mmhB_LF, cum_leach_30mmh_LF,
+                    cum_leach_135mmh_LA, cum_leach_55mmhA_LA, cum_leach_55mmhB_LA, cum_leach_30mmh_LA)
+
+                time_size_135mmh = water_data[0][:, 33]
+                time_size_55mmhA = water_data[0][:, 34]
+                time_size_55mmhB = water_data[0][:, 35]
+                time_size_30mmh = water_data[0][:, 36]
+
+                time_sizes2 = [time_size_135mmh, time_size_135mmh,
+                               time_size_55mmhA, time_size_55mmhA,
+                               time_size_55mmhB, time_size_55mmhB,
+                               time_size_30mmh, time_size_30mmh]
+
+                return hydroplot3(
+                    percol_data2,
+                    series_name1, series_name2, series_name3, series_name4,
+                    leach_high_6min, leach_med_12min, leach_med_30min, leach_low_30min,
+                    title
+                )
+            else:
+                # Observed ponding
+                # [sterile, untreat, sterile_aged, untreat_aged]
+                # all at 6 min, high inetnesity
+                roff_high_6min = np.array([10.824, 20.935, 24.75, 19.041])
+
+                # all at 12 min, med intensity
+                roff_med_12min = np.array([0, 3.907, 19.436, 7.313])
+
+                # all at 30min, med intensity
+                roff_med_30min = np.array([43.764, 28.911, 51.964, 33.478])
+
+                # all at 30min, low intensity
+                roff_low_30min = np.array([0, 22.618, 28.598, 27.314])
+
+                # Time axis
+                cum_time_30min = water_data[0][:, 0]
+
+                # Ponding cumulative
+                cum_roff_135mmh_SF = water_data[0][:, 1]
+                cum_roff_135mmh_SA = water_data[0][:, 3]
+                cum_roff_135mmh_LF = water_data[0][:, 5]
+                cum_roff_135mmh_LA = water_data[0][:, 7]
+
+                cum_roff_55mmhA_SF = water_data[0][:, 9]
+                cum_roff_55mmhA_SA = water_data[0][:, 11]
+                cum_roff_55mmhA_LF = water_data[0][:, 13]
+                cum_roff_55mmhA_LA = water_data[0][:, 15]
+
+                cum_roff_55mmhB_SF = water_data[0][:, 17]
+                cum_roff_55mmhB_SA = water_data[0][:, 19]
+                cum_roff_55mmhB_LF = water_data[0][:, 21]
+                cum_roff_55mmhB_LA = water_data[0][:, 23]
+
+                cum_roff_30mmh_SF = water_data[0][:, 25]
+                cum_roff_30mmh_SA = water_data[0][:, 27]
+                cum_roff_30mmh_LF = water_data[0][:, 29]
+                cum_roff_30mmh_LA = water_data[0][:, 31]
+
+                runoff_data2 = stackdata16(
+                    cum_time_30min,
+                    cum_roff_135mmh_SF, cum_roff_55mmhA_SF, cum_roff_55mmhB_SF, cum_roff_30mmh_SF,
+                    cum_roff_135mmh_SA, cum_roff_55mmhA_SA, cum_roff_55mmhB_SA, cum_roff_30mmh_SA,
+                    cum_roff_135mmh_LF, cum_roff_55mmhA_LF, cum_roff_55mmhB_LF, cum_roff_30mmh_LF,
+                    cum_roff_135mmh_LA, cum_roff_55mmhA_LA, cum_roff_55mmhB_LA, cum_roff_30mmh_LA)
+
+                time_size_135mmh = water_data[0][:, 33]
+                time_size_55mmhA = water_data[0][:, 34]
+                time_size_55mmhB = water_data[0][:, 35]
+                time_size_30mmh = water_data[0][:, 36]
+
+                time_sizes2 = [time_size_135mmh, time_size_135mmh,
+                               time_size_55mmhA, time_size_55mmhA,
+                               time_size_55mmhB, time_size_55mmhB,
+                               time_size_30mmh, time_size_30mmh]
+
+                return hydroplot3(
+                    runoff_data2,
+                    series_name1, series_name2, series_name3, series_name4,
+                    roff_high_6min, roff_med_12min, roff_med_30min, roff_low_30min,
+                    title)
+
+
 def hydroplot3(
         data,
         y1, y2, y3, y4,
